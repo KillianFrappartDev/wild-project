d0343affa873f6dc1c71a4e39d952497533dcb63
Killian Frappart <Killianfrappartdev@gmail.com>
2020-06-21 18:58:46 +0200

    Finished base project

--- script.js
+++ script.js
@@ -47,12 +47,16 @@ setInterval(() => {
   carouselItems[currentIndex].style.zIndex += 1;
 }, 8000);
 
-//ROBOT
+//Chat
 // ---> Variables
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const messageInput = document.getElementById("message");
 const submitBtn = document.querySelector(".contact__form-button");
+const chat = document.querySelector(".chat");
+const chatBox = document.querySelector(".chat__messages");
+const chatBtn = document.querySelector(".chat__input-btn");
+const userInput = document.querySelector(".chat__input");
 let userName;
 
 // ---> Event Listeners
@@ -66,12 +70,39 @@ submitBtn.addEventListener("click", () => {
     nameInput.value = "";
     emailInput.value = "";
     messageInput.value = "";
+    chat.classList.add("visible-chat");
 
-    getJoke();
+    const welcomeMsg = document.createElement("div");
+    welcomeMsg.classList.add("chat__robot-text");
+    welcomeMsg.innerHTML = "Hello " + userName + " your message has been successfully sent ! ✔";
+    setTimeout(() => {
+        chatBox.appendChild(welcomeMsg);
+    }, 1000)
+
+    const secondMsg = document.createElement("div");
+    secondMsg.classList.add("chat__robot-text");
+    secondMsg.innerHTML = "I'm a dummy robot, I can only tell jokes... Do you want to laugh ? ";
+    setTimeout(() => {
+        chatBox.appendChild(secondMsg);
+    }, 2000)
 
   } else {
     alert("Inputs can not be empty, please type something first");
   }
+});
+
+chatBtn.addEventListener("click", () => {
+    if (userInput.value.trim()) {
+        const userMsg = userInput.value;
+        const newMsg = document.createElement("div");
+        newMsg.classList.add("chat__user-text");
+        newMsg.innerHTML = userMsg;
+        chatBox.appendChild(newMsg);
+        userInput.value = "";
+        setTimeout(() => {
+            getJoke();
+        }, 1000)
+    }
 });
 
 // ---> Functions
@@ -87,7 +118,10 @@ function getJoke() {
       return response.json();
     })
     .then((jokeObj) => {
-        console.log(jokeObj.content);
+        const newJoke = document.createElement("div");
+        newJoke.classList.add("chat__robot-text");
+        newJoke.innerHTML = jokeObj.content;
+        chatBox.appendChild(newJoke);
     })
     .catch((err) => {
       console.log(err);
